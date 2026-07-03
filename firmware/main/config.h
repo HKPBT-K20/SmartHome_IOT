efbfc617fee279136e9f1998ada125037b2e63de
PhuVo0112 <rggaming1235555@gmail.com>
2026-07-03 07:52:49 +0700

    feat: initialize dashboard application and add firmware configuration file

--- firmware/main/config.h
+++ firmware/main/config.h
@@ -3,9 +3,8 @@
 
 #define WIFI_SSID     "your_wifi"                             // Tên wifi
 #define WIFI_PASS     "your_password"                         // Mật khẩu wifi
-#define FIREBASE_URL  "https://smarthomeiot-69529-default-rtdb.asia-southeast1.firebasedatabase.app"
-#define FIREBASE_KEY  "AIzaSyBIIUyjFO2dKdogDAM6oZ6E5H4WPtSk0aM"
-#define WEATHER_KEY   "your_openweather_key"                  // API key OpenWeatherMap
+#define FIREBASE_URL  "https://smart-home-iot-d1c77-default-rtdb.asia-southeast1.firebasedatabase.app"
+#define FIREBASE_KEY  "AIzaSyDo0ncLdnDplJoessMju0EKoGQGsvDealI"         // API key 
 #define CITY_ID       "1580578"                               // TP.HCM
 
 #endif // CONFIG_H
